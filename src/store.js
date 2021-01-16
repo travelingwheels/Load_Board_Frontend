46527cbd6fba9770def4e92e6c525ff35f579a26
Gary <gkwheeler40@gmail.com>
2021-01-15 18:57:27 -0700

    import loginForm and currentUser reducers and add them to redcer variable

--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import users from './reducers/users.js'
+import currentUser from './reducers/currentUser.js'
+import loginForm from './reducers/loginForm.js'
 
 const reducer = combineReducers({
-    users
+    users,
+    currentUser,
+    loginForm
   })
   
   const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
