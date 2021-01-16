4a3ba5300e4151354f2b3abed3d2b0cfd49ec2c1
Gary <gkwheeler40@gmail.com>
2021-01-15 18:59:51 -0700

    import connect and getCurrentUser, add componentDidMount function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login.js'
+import { connect } from 'react-redux'
+import { getCurrentUser } from './actions/currentUser.js'
 
 class App extends React.Component {
   
+  componentDidMount() {
+    this.props.getCurrentUser()
+  }
+
   render() {
     return (
-      "Hello from React!"
+      <Login/>
     );
   }
 }
 
-export default App;
+export default connect(null, { getCurrentUser })(App);
