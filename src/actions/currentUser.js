1557e8b2979172ab527695bc0f1b31a7682a4ce0
Gary <gkwheeler40@gmail.com>
2021-01-15 19:13:45 -0700

    add setCurrentUser, login, and getCurrentUser actions

---
+++ src/actions/currentUser.js
@@ -0,0 +1,51 @@
+export const setCurrentUser = user => {
+    return {
+        type: "SET_CURRENT_USER",
+        user
+    }
+}
+
+// asychronous action creators
+export const login = credentials => {
+    console.log("credentials are", credentials)
+    return dispatch => {
+        return fetch("http://localhost:3001/api/v1/login", {
+            credentials: "include",
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(credentials)
+        })
+        .then(res => res.json())
+        .then(user => {
+            if (user.error) {
+                alert(user.error)
+            } else {
+                dispatch(setCurrentUser(user))
+            }
+        })
+        .catch(console.log)
+    }
+}
+
+export const getCurrentUser = () => {
+    return dispatch => {
+        return fetch("http://localhost:3001/api/v1/get_current_user", {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                "Content-type": "application/json"
+            },
+        })
+        .then(res => res.json())
+        .then(user => {
+            if (user.error) {
+                alert(user.error)
+            } else {
+                dispatch(setCurrentUser(user))
+            }
+        })
+        .catch(console.log)
+    }
+}
