02663f87923c107a779d18b8adafa43859ce5c00
Gary <gkwheeler40@gmail.com>
2021-01-17 18:53:43 -0700

    add the logout function

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -5,6 +5,12 @@ export const setCurrentUser = user => {
     }
 }
 
+export const clearCurrentUser = () => {
+    return {
+        type: "CLEAR_CURRENT_USER"
+    }
+}
+
 // asychronous action creators
 export const login = credentials => {
     console.log("credentials are", credentials)
@@ -26,6 +32,16 @@ export const login = credentials => {
             }
         })
         .catch(console.log)
+    }
+}
+
+export const logout = () => {
+    return dispatch => {
+        dispatch(clearCurrentUser())
+        return fetch('http://localhost:3001/api/v1/logout', {
+            credentials: "include",
+            method: "DELETE"
+        })
     }
 }
 
