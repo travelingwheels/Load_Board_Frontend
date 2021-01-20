a745fc81409176e85e3284ae853835d14f1ed3de
Gary <gkwheeler40@gmail.com>
2021-01-19 18:10:34 -0700

    add button from react-bootstrap and implement

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { logout } from '../actions/currentUser.js'
+import Button from 'react-bootstrap/Button'
 
 
 const Logout = ({ logout }) => {
 
       return (
         <form onSubmit={logout}>
-          <input type="submit" value="Log Out"/>
+          {/* <input type="submit" value="Log Out"/> */}
+          <Button as="input" type="submit" value="logout" />
         </form>
       )
     }
