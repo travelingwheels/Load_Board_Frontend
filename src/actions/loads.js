421b553fa5eda39a09566a8880bfbcf72f451cee
Gary <gkwheeler40@gmail.com>
2021-01-23 22:57:03 -0700

    create a setLoads action

---
+++ src/actions/loads.js
@@ -0,0 +1,29 @@
+// synchronous actions
+export const setLoads = loads => {
+    return {
+        type: "SET_LOADS",
+        loads
+    }
+}
+
+// asynchronous actions
+export const getLoads = () => {
+    return dispatch => {
+        return fetch("http://localhost:3001/api/v1/loads", {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                "Content-type": "application/json"
+            },
+        })
+        .then(res => res.json())
+        .then(response => {
+            if (response.error) {
+                alert(response.error)
+            } else {
+                dispatch(setLoads)
+            }
+        })
+        .catch(console.log)
+    }
+}
