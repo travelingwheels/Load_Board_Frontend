17b908eda5ba0dc300b17709cf1d48f609c75abd
Gary <gkwheeler40@gmail.com>
2021-01-23 22:54:55 -0700

    create MainContainer component

---
+++ src/components/MainContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+
+
+const MainContainer = () => {
+    return (
+        <Container className="App">
+        "Hello in the maincontainer"
+        </Container>
+    )
+}
+
+export default MainContainer
