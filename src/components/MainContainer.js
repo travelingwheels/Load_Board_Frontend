0d3142676a5a0cac6ff9e2317405aa3b471bfed1
Gary <gkwheeler40@gmail.com>
2021-01-26 20:30:32 -0700

    import Loads component

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container';
+import Loads from './Loads.js'
 
 
 const MainContainer = () => {
     return (
         <Container className="App">
-        "Hello in the maincontainer"
+            <Loads/>
         </Container>
     )
 }
