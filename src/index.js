6abc4ba395d4dd6aec1fd055000bb7dcf46833d9
Gary <gkwheeler40@gmail.com>
2021-01-30 21:12:42 -0700

    add BrowserRouter and wrap App with it

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import store from './store.js'
+import { BrowserRouter } from 'react-router-dom'
 
 
 ReactDOM.render(
   <React.StrictMode>
-  <Provider store={ store }><App /></Provider>
+  <Provider store={ store }><BrowserRouter><App /></BrowserRouter></Provider>
   </React.StrictMode>, 
   document.getElementById('root')
 );
