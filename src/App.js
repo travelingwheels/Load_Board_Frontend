0efa26de6f78cb86714b9ccfdbacc43cfaabfd0c
Gary <gkwheeler40@gmail.com>
2021-02-06 14:18:05 -0700

    add mapStateToProps and changes to layout

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser.js'
 import NavBar from './components/NavBar.js'
 import Logout from './components/Logout.js'
+import Login from './components/Login.js'
 import Loads from './components/Loads.js'
+import  Home from './components/Home.js'
 import MainContainer from './components/MainContainer.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route } from 'react-router-dom'
+import { Route, Switch, withRouter } from 'react-router-dom'
 import Container from 'react-bootstrap/Container';
 
 class App extends React.Component {
@@ -18,16 +20,25 @@ class App extends React.Component {
   }
 
   render() {
+    const { loggedIn } = this.props
     return (
       <Container className="App">
-        <Logout/>
-      <NavBar/>
-      <Route exact path='/loads' component={Loads}/>
+        { loggedIn ? <NavBar {...this.props}/> : <Home/> }
+        <Switch>
+      <Route exact path='/login' component={Login}/>
+      {/* <Route exact path='/home' component={Home}/> */}
+      </Switch>
       <MainContainer/> 
+        <Logout/>
       </Container>
     );
   }
 }
 
+const mapStateToProps = state => {
+  return ({
+    logginIn: !!state.currentUser
+  })
+}
 
-export default connect(null, { getCurrentUser })(App);
+export default withRouter(connect(mapStateToProps, { getCurrentUser })(App));
