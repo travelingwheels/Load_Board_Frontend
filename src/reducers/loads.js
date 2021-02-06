d45d648d7c13b4ddf852bf30146fc88fdd0dc642
Gary <gkwheeler40@gmail.com>
2021-02-06 14:02:52 -0700

    add clearLoads case statement

--- src/reducers/loads.js
+++ src/reducers/loads.js
@@ -1,7 +1,11 @@
+const initialState = []
+
 export default (state = [], action) => {
     switch (action.type) {
         case "SET_LOADS":
             return action.loads
+         case "CLEAR_LOADS":
+            return initialState
         default:
             return state
     }
