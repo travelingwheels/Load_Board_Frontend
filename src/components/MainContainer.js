337596e956e450b86c9ddb755ef3eb874ea8f53e
Gary <gkwheeler40@gmail.com>
2021-02-10 09:24:14 -0700

    import and directions component and add it to MainContainer

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Loads from './Loads.js'
+import Directions from './Directions.js'
 
 
 const MainContainer = () => {
     return (
         <Container className="App">
             <Loads/>
+            <Directions/>
         </Container>
     )
 }
