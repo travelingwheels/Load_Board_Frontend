594e16f0ade08df38dcf3ebff73bb1f86dd33c16
Gary <gkwheeler40@gmail.com>
2021-02-18 16:47:34 -0700

    add link to Directions

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Loads from './Loads.js'
 import Directions from './Directions.js'
+import { Link } from 'react-router-dom'
 
 
 const MainContainer = () => {
     return (
         <Container className="App">
             <Loads/>
-            <Directions/>
+            <Link to="/directions">Directions</Link>
         </Container>
     )
 }
