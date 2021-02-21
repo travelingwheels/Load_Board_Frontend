ecdd1df77fd8557440b2143bd30627e3ce577f06
Gary <gkwheeler40@gmail.com>
2021-02-21 09:25:47 -0700

    add state and mapStateToProps

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Loads from './Loads.js'
-import Directions from './Directions.js'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 
-const MainContainer = () => {
+const MainContainer = ({  loggedIn }) => {
     return (
         <Container className="App">
+             { loggedIn ? <>
             <Loads/>
             <Link to="/directions">Directions</Link>
+            </> : null}
         </Container>
     )
 }
 
-export default MainContainer
+const mapStateToProps = ({ currentUser }) => {
+    return {
+        currentUser,
+        loggedIn: !!currentUser
+    }
+}
+
+export default  connect(mapStateToProps)( MainContainer)
