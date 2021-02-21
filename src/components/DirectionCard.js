5bbfbaef0b38d05e37219ee4ecdaee72813f940a
Gary <gkwheeler40@gmail.com>
2021-02-21 09:28:25 -0700

    add link to comments

--- src/components/DirectionCard.js
+++ src/components/DirectionCard.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const DirectionCard = ({ direction }) => {
     return (
         <>
         <p>{direction.attributes.content}</p>
+        <Link to="/comments">Comments</Link>
         </>
     )
 }
