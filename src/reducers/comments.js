f2ac0e51313fffbce8b737f959970f7d377e8cc0
Gary <gkwheeler40@gmail.com>
2021-02-22 19:52:04 -0700

    create reducer for comments component

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,7 +1,11 @@
+const initialState = []
+
 export default (state = [], action) => {
     switch (action.type) {
         case "SET_COMMENTS":
             return action.comments
+        case "CLEAR_COMMENTS":
+            return initialState
         default:
             return state
     }
