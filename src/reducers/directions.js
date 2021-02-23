5e9f7bb68af7cbe51bdc203246de5809e1216ad7
Gary <gkwheeler40@gmail.com>
2021-02-22 19:53:54 -0700

    add clearDirections case statement and retun initialState

--- src/reducers/directions.js
+++ src/reducers/directions.js
@@ -1,9 +1,11 @@
-// const initialState = []
+const initialState = []
 
 export default (state = [], action) => {
     switch (action.type) {
         case "SET_DIRECTIONS":
             return action.directions
+        case "CLEAR_DIRECTIONS":
+            return initialState
         default:
             return state
     }
