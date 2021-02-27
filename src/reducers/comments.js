cf4a144740cac7bbfd94a4578430b6e5cadb91b4
Gary <gkwheeler40@gmail.com>
2021-02-26 23:16:50 -0700

    add update and delete case to reducer

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -6,6 +6,10 @@ export default (state = [], action) => {
             return action.comments
         case "ADD_COMMENT":
             return state.comments.concat(action.comments)
+         case "UPDATE_COMMENT":
+            return state.map(comment => comment.id === action.comment.id ? action.comment : comment)
+        case "DELETE_ COMMENT":
+            return state.filter(comment => comment.id === action.commentId ? false : true)
         case "CLEAR_COMMENTS":
             return initialState
         default:
