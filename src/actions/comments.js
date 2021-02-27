7eadb1b4dc30a09eaf992ae444620079ba20e862
Gary <gkwheeler40@gmail.com>
2021-02-26 20:30:38 -0700

    import resetCommentForm grom commentForm, add createComment action and add comment

--- src/actions/comments.js
+++ src/actions/comments.js
@@ -1,8 +1,17 @@
+import { resetCommentForm } from './commentForm'
+
 // synchronous
 export const setComments =comments => {
     return {
         type: "SET_COMMENTS",
        comments
+    }
+}
+
+export const addComment = comment => {
+    return {
+        type: "ADD_COMMENT",
+        comment
     }
 }
 
@@ -28,6 +37,34 @@ export const getComments = () => {
                 alert(response.error)
             } else {
                 dispatch(setComments(response.data))
+            }
+        })
+        .catch(console.log)
+    }
+}
+
+export const createComment = (commentData, history) => {
+    return dispatch => {
+        const sendCommentData = {
+            content: commentData.content,
+            user_id: commentData.userId
+        }
+        return fetch("http://localhost:3001/api/v1/comments", {
+            credentials: "include",
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body : JSON.stringify(sendCommentData)
+        })
+        .then(response => response.json())
+        .then(resp => {
+            if (resp.error) {
+                alert(resp.error)
+            } else {
+                dispatch(addComment(resp.data))
+                dispatch(resetCommentForm())
+                history.push(`/comments/${resp.data.id}`)
             }
         })
         .catch(console.log)
