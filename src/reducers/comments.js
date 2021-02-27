aed24ab53f79707ff45c404af82902069a39d1d5
Gary <gkwheeler40@gmail.com>
2021-02-26 20:34:29 -0700

    add ADD_COMMENT case statement

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -4,6 +4,8 @@ export default (state = [], action) => {
     switch (action.type) {
         case "SET_COMMENTS":
             return action.comments
+        case "ADD_COMMENT":
+            return state.comments.concat(action.comments)
         case "CLEAR_COMMENTS":
             return initialState
         default:
