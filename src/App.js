3075eb336d27b26980ddd051f6f52eab446cf6da
Gary <gkwheeler40@gmail.com>
2021-02-26 20:27:28 -0700

    delete unused imports, import commentForm, and adjusted react router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser.js'
 import NavBar from './components/NavBar.js'
-import Logout from './components/Logout.js'
+import CommentForm from './components/CommentForm.js'
 import Login from './components/Login.js'
-import Loads from './components/Loads.js'
 import  Home from './components/Home.js'
 import  Directions from './components/Directions.js'
 import  Comments from './components/Comments.js'
 import MainContainer from './components/MainContainer.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch, withRouter } from 'react-router-dom'
-import Container from 'react-bootstrap/Container';
+
 
 class App extends React.Component {
   
@@ -28,11 +26,12 @@ class App extends React.Component {
         { loggedIn  ?  <NavBar location={this.props.location}/> : <Home/> }
        
         <Switch>
-      <Route exact path='/login' component={Login}/>
-      <Route exact path='/' render={()=> loggedIn ? <MainContainer/> : null}/>
-      <Route  path='/directions' component={Directions}/>
-      <Route  path='/comments' component={Comments}/>
-      </Switch>
+             <Route exact path='/login' component={Login}/>
+             <Route exact path='/' render={()=> loggedIn ? <MainContainer/> : null}/>
+             <Route  exact path='/directions' component={Directions}/>
+             <Route  exact path='/comments' component={Comments}/>
+             <Route  exact path='/comments/new' component={CommentForm}/>
+         </Switch>
       </div>
     );
   }
