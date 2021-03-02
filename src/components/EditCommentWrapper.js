0d5a0a4e0798575d295a8add3fa250d018ddf6f2
Gary <gkwheeler40@gmail.com>
2021-03-01 23:01:40 -0700

    add handleSubmit function

--- src/components/EditCommentWrapper.js
+++ src/components/EditCommentWrapper.js
@@ -1,7 +1,27 @@
 import React from "react";
+import CommentForm from './CommentForm'
+import { updateComment, deleteComment } from '../actions/myComments'
+import { setFormDataForEdit, resetCommentForm } from '../actions/commentForm'
+import { connect } from 'react-redux'
 
 class EditCommentWrapper extends React.Component {
     componentDidMount(){
         this.props.comment && this.props.setFormDataForEdit(this.props.comment)
     }
+
+    componentDidUpdate(prevProps) {
+        this.props.comment && !prevProps.comment && this.props.setFormDataForEdit(this.props.comment)
+      }
+    
+      componentWillUnmount() {
+        this.props.resetCommentForm()
+      }
+
+      handleSubmit = (formData) => {
+        const { updateComment, comment, history } = this.props
+        updateComment({
+          ...formData,
+          commentId: comment.id
+        }, history)
+      }
 }
