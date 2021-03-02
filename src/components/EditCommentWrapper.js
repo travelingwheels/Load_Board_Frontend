81787466e8f83275ab7baab479fc2baff7665b9b
Gary <gkwheeler40@gmail.com>
2021-03-01 22:58:22 -0700

    add component lifcycle methods

--- src/components/EditCommentWrapper.js
+++ src/components/EditCommentWrapper.js
@@ -2,6 +2,6 @@ import React from "react";
 
 class EditCommentWrapper extends React.Component {
     componentDidMount(){
-        this.props.trip && this.props.setFormDataForEdit(this.props.trip)
+        this.props.comment && this.props.setFormDataForEdit(this.props.comment)
     }
 }
