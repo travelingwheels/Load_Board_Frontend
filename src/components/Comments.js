1acde0ebe86f49ffa52de84107a0927dfd579e3c
Gary <gkwheeler40@gmail.com>
2021-03-06 19:56:48 -0700

    change in format and add direction to mapStateToProps

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -2,16 +2,21 @@ import React from 'react'
 import CommentCard from './CommentCard.js'
 import { connect } from 'react-redux'
 
-const Comments = props => {
-    const commentCards = props.comments.length > 0 ? props.comments.map(c => <CommentCard comment={c} key={c.id}/>) : null
+const Comments = ({ comments, direction }) => {
+    const visibleComments = comments.filter(comment => comment.relationships.direction.data.id == direction);
+    console.log({direction})
+    const commentCards = visibleComments.length > 0
+        ? visibleComments.map(c => <CommentCard comment={c} key={c.id}/>)
+        : null
     return (
         commentCards
     )
 }
 
-const mapStateToProps = ({ comments }) => {
+const mapStateToProps = ({ comments, directions }) => {
     return {
-        comments
+        comments,
+        direction: directions.selectedDirection
     }
 }
 
