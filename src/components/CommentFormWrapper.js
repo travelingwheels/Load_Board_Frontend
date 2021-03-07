62386f95cb9ad2ef76170ecb006fe7eb964169d2
Gary <gkwheeler40@gmail.com>
2021-03-06 19:54:58 -0700

    add directions and Id from props, also to mapStateToProps

--- src/components/CommentFormWrapper.js
+++ src/components/CommentFormWrapper.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import CommentForm from './CommentForm'
 import { createComment } from '../actions/comments'
+import { getDirections } from '../actions/directions'
 import { connect } from 'react-redux'
 
-const CommentFormWrapper = ({ history, createComment }) => {
+const CommentFormWrapper = ({ history, createComment, directions, directionId }) => {
+  //getDirections();
+
+  console.log({ directions })
 
   const handleSubmit = (formData, userId, directionId) => {
+    console.log({ directionId })
     createComment({
       ...formData,
       userId,
       directionId
     }, history)
   }
-  return  <CommentForm history={history} handleSubmit={handleSubmit} />
+
+  return <CommentForm history={history} handleSubmit={handleSubmit} />
 };
 
-export default connect(null, { createComment })(CommentFormWrapper);
+const mapStateToProps = ({ directions }) => {
+  return ({
+    directions,
+    directionId: directions.selectedDirection
+  })
+}
+export default connect(mapStateToProps, { createComment, getDirections })(CommentFormWrapper);
