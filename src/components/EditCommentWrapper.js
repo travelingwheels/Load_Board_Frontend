b6c6b939068a558730f7ce46dfd3f0751fa67c4c
Gary <gkwheeler40@gmail.com>
2021-03-08 00:12:39 -0700

    add functions for CRUD functionality, and dispatch

--- src/components/EditCommentWrapper.js
+++ src/components/EditCommentWrapper.js
@@ -1,39 +1,45 @@
 import React from "react";
 import CommentForm from './CommentForm'
-import { updateComment, deleteComment } from '../actions/myComments'
+import { updateComment, deleteComment } from '../actions/comments'
 import { setFormDataForEdit, resetCommentForm } from '../actions/commentForm'
 import { connect } from 'react-redux'
 
 class EditCommentWrapper extends React.Component {
-    componentDidMount(){
+    componentDidMount() {
         this.props.comment && this.props.setFormDataForEdit(this.props.comment)
     }
 
     componentDidUpdate(prevProps) {
         this.props.comment && !prevProps.comment && this.props.setFormDataForEdit(this.props.comment)
-      }
-    
-      componentWillUnmount() {
+    }
+
+    componentWillUnmount() {
         this.props.resetCommentForm()
-      }
+    }
 
-      handleSubmit = (formData) => {
-        const { updateComment, comment, history } = this.props
+    handleSubmit = (formData, userId, directionId) => {
+        const { updateComment, comment, comment: { commentId }, history } = this.props;
+
+
         updateComment({
-          ...formData,
-          commentId: comment.id
+            ...formData,
+            userId,
+            commentId: comment.id
         }, history)
-      }
+    }
 
-      render() {
+    render() {
         const { history, deleteComment, comment } = this.props
-        const commentId = comment ? comment.id : null
-        return  <>
-                  <CommentForm editMode handleSubmit={this.handleSubmit} />
-                  <br/>
-                  <button style={{color: "red"}} onClick={()=>deleteComment(commentId, history)}>Delete this Comment</button>
-                </>
-      }
+        const commentId = comment?.id;
+        
+        return (
+            <>
+                <CommentForm editMode handleSubmit={this.handleSubmit} />
+                <br />
+                <button style={{ color: "red" }} onClick={() => deleteComment(commentId, history)}>Delete this Comment</button>
+            </>
+        )
+    }
 };
 
 export default connect(null, { updateComment, setFormDataForEdit, resetCommentForm, deleteComment })(EditCommentWrapper);
